72a0a4f0d4259a2d2aa9229f50dde2790974fd9b
Libra-lsp <lishipeng1012@outlook.com>
2025-04-24 16:08:22 +0800

    update

--- src/uitls/Utils.ts
+++ src/uitls/Utils.ts
@@ -4,6 +4,7 @@ import { Example } from '@/Defines'
 
 export const fetchExamples = async () => {
   return new Promise<Example[]>((resolve) => {
+    console.log(process.env.BASE_URL);
     fetch(`${process.env.BASE_URL}${ExamplesFileUrl}`).then(response => response.json()).then(data => {
       resolve(data)
     })
